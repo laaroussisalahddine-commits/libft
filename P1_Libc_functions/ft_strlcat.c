0b5afccbf8b828f6ad922f524f8880c99fb467b3
laaroussi <laaroussisalahddine@example.com>
2026-09-13 18:25:09 +0100

    x

---
+++ P1_Libc_functions/ft_strlcat.c
@@ -0,0 +1,34 @@
+#include <string.h>
+
+size_t ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}  
+
+size_t ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t 	i;
+	size_t 	j;
+	size_t 	ldest;
+	size_t 	lsrc;
+
+	ldest = ft_strlen(dst);
+	lsrc = ft_strlen(src);
+	if (size <= ldest)
+		return (size + lsrc);
+	i = ldest; 
+	j = 0;
+	while (i < size - 1 && src[j] != '\0')
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (ldest + lsrc);
+}
