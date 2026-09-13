58837c03d2c72bbddb60c45a357b7b136fa8cc6a
laaroussi <laaroussisalahddine@example.com>
2026-09-13 18:46:18 +0100

    add libft.h

--- P1_Libc_functions/ft_strncmp.c
+++ P1_Libc_functions/ft_strncmp.c
@@ -12,15 +12,3 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
     
     return((unsigned char)s1[i] -(unsigned char)s2[i]);
 }
-
-#include<stdio.h>
-int main(void)
-{
-    char s1[] = "alah";
-    char s2[] = "salah";
-
-    int res;
-
-    res = ft_strncmp(s1,s2,32);
-    printf("%d\n",res);
-}
